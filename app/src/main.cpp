8236a874006244f3b8e5bb695bc1e842cc073dd4
NikFri94 <139549862+NikFri94@users.noreply.github.com>
2024-05-10 14:41:36 +0200

    Project Initialisation

---
+++ app/src/main.cpp
@@ -0,0 +1,18 @@
+/*!
+ *
+ *  @file      main.cpp
+ *  @brief     Haupteinstiegspunkt der Anwendung
+ *  @details   ~
+ *  @author    Niklas Frings
+ *  @date      10.05.2024
+ *  @copyright © Niklas Frings, 2024. All right reserved.
+ *
+ */
+#include <QApplication>
+
+int main(int argc, char* argv[])
+{
+	QApplication app(argc, argv);
+
+	return app.exec();
+}
---
+++ cmake/UseDoxygenDoc.cmake
@@ -0,0 +1,37 @@
+# (C) Niklas Frings, 2023
+# University of Wuppertal
+# Makefile für die Nutzung von Doxygen zur Dokumentation
+# des Codes
+find_package(Perl REQUIRED)
+find_package(Doxygen REQUIRED)
+
+function(add_doxygen_doc)
+  set(options)
+  set(oneValueArgs BUILD_DIR DOXY_FILE TARGET_NAME COMMENT)
+  set(multiValueArgs)
+
+  cmake_parse_arguments(DOXY_DOC
+    "${options}"
+    "${oneValueArgs}"
+    "${multiValueArgs}"
+    ${ARGN}
+    )
+
+  configure_file(
+    ${DOXY_DOC_DOXY_FILE}
+    ${DOXY_DOC_BUILD_DIR}/Doxyfile
+    @ONLY
+    )
+
+  add_custom_target(${DOXY_DOC_TARGET_NAME}
+    COMMAND
+      ${DOXYGEN_EXECUTABLE} Doxyfile
+    WORKING_DIRECTORY
+      ${DOXY_DOC_BUILD_DIR}
+    COMMENT
+      "Building ${DOXY_DOC_COMMENT} with Doxygen"
+    VERBATIM
+    )
+
+  message(STATUS "Added ${DOXY_DOC_TARGET_NAME} [Doxygen] target to build documentation")
+endfunction()
