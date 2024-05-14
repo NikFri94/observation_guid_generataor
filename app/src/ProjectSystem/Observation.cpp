fa227e03a4db652ec2817d79898b62d4915c2fa7
NikFri94 <139549862+NikFri94@users.noreply.github.com>
2024-05-14 15:57:34 +0200

    + Added new class for holding observation information ~ Adjusted "New Observation" Dialog

--- app/src/ProjectSystem/Observation.cpp
+++ app/src/ProjectSystem/Observation.cpp
@@ -11,5 +11,40 @@
 #include "Observation.h"
 
 namespace OGG {
-
+	/*!
+	 * @brief Implementation of the default initialisation constructor
+	*/
+	Observation::Observation(QString _projectName, 
+		QDate _date, 
+		QString _location, 
+		QString _mainObjective, 
+		QString _excluded, 
+		QString _observationObjectDescription, 
+		bool _isParticipational, 
+		bool _isUndercover, 
+		bool _isSystematic, 
+		bool _isFieldObservation) : m_projectName(_projectName), 
+									m_date(_date),
+									m_locatiom(_location),
+									m_MainObjective(_mainObjective),
+									m_exclude(_excluded),
+									m_observationObjectDescription(_observationObjectDescription),
+									m_isParticipational(_isParticipational),
+									m_isUndercover(_isUndercover),
+									m_isSystematic(_isSystematic),
+									m_isFieldObservation(_isFieldObservation)
+	{
+		// Debug-Log-Output
+		qDebug() << "Created new observation:";
+		qDebug() << QString("\t\tProject name:\t{1}").arg(this->m_projectName);
+		qDebug() << QString("\t\tDate:\t{1}").arg(this->m_date.toString(Qt::DateFormat::ISODate));
+		qDebug() << QString("\t\tLocation:\t{1}").arg(this->m_locatiom);
+		qDebug() << QString("\t\tMain objective:\t{1}").arg(this->m_MainObjective);
+		qDebug() << QString("\t\tExclude:\t{1}").arg(this->m_exclude);
+		qDebug() << QString("\t\tObservation object description:\t{1}").arg(this->m_observationObjectDescription);
+		qDebug() << QString("\t\tIs Participational:\t{1}").arg(this->m_isParticipational?"TRUE":"FALSE");
+		qDebug() << QString("\t\tIs Undercover:\t{1}").arg(this->m_isUndercover ? "TRUE" : "FALSE");
+		qDebug() << QString("\t\tIs systematically:\t{1}").arg(this->m_isSystematic ? "TRUE" : "FALSE");
+		qDebug() << QString("\t\tIs field observation:\t{1}").arg(this->m_isFieldObservation ? "TRUE" : "FALSE");
+	}
 }
--- app/src/ProjectSystem/Observation.h
+++ app/src/ProjectSystem/Observation.h
@@ -11,6 +11,7 @@
 #pragma once
 
 #include <QString>
+#include <QDate>
 
 namespace OGG
 {
@@ -20,8 +21,77 @@ namespace OGG
 	class Observation
 	{
 	public:
+		/*!
+		 * @brief Default constructor for initialisation
+		 * @param _projectName
+		 * @param _date
+		 * @param _location
+		 * @param _mainObjective
+		 * @param _excluded
+		 * @param _observationObjectDescription
+		 * @param _isParticipational
+		 * @param _isUndercover
+		 * @param _isSystematic
+		 * @param _isFieldObservation
+		*/
+		Observation(QString _projectName,
+			QDate _date,
+			QString _location,
+			QString _mainObjective,
+			QString _excluded,
+			QString _observationObjectDescription,
+			bool _isParticipational = false,
+			bool _isUndercover = false,
+			bool _isSystematic = true,
+			bool _isFieldObservation = true);
+
+		/*
+		* Zugriffsmethoden
+		*/
+		QString getProjectName() const {
+			return this->m_projectName;
+		}
+		QDate getDate() const {
+			return this->m_date;
+		}
+		QString getLocation() const {
+			return this->m_locatiom;
+		}
+		QString getMainObjective() const {
+			return this->m_MainObjective;
+		}
+		QString getExcluded() const {
+			return this->m_exclude;
+		}
+		QString getObservationObjectDescription() const {
+			return this->m_observationObjectDescription;
+		}
+		bool isParticipational() const {
+			return this->m_isParticipational;
+		}
+		bool isUndervocer() const {
+			return this->m_isUndercover;
+		}
+		bool isSystematic() const {
+			return this->m_isSystematic;
+		}
+		bool isFieldObservation() const {
+			return this->m_isFieldObservation;
+		}
 
 	private:
-		QString m_projectName;
+		QString m_projectName; //!< Name of the project in which context the observation takes place
+
+		QDate	m_date; //!< Date of the observation
+		QString	m_locatiom; //!< Location, in which the observation takes place
+
+		QString	m_MainObjective;	 //!< Describes the main goal of the observation
+		QString	m_exclude;	 //!< What should NOT be observed?
+		QString	m_observationObjectDescription; //!< Brief description of the Object or Person which should be observed.
+
+		bool	m_isParticipational;	 //!<  True if the observation is in participal mode
+		bool	m_isUndercover;		 //!< False if the observation is covered, true for open observations
+		bool	m_isSystematic;	 //!< True for systematic observations
+		bool	m_isFieldObservation; //!< True if this is a field observation, false for laboritory
 	};
 }
